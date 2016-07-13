5cb6a0055eb67c402389816169a88a7bfc6dc73b
CodeingBoy <codeingboy@gmail.com>
2016-07-13 17:28:15 +0800

    now server can send simple response to client

--- src/server/server.java
+++ src/server/server.java
@@ -1,9 +1,9 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import http.HTTPRequest;
+import http.HTTPResponse;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -36,6 +36,16 @@ public class Server {
         server.start();
     }
 
+    public static void showContent(String title, String content) {
+        String head = "----------" + title + "----------";
+        System.out.println(head);
+        System.out.println(content);
+        for (int i = 0; i < head.length(); i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
     public void start() {
         try {
             serverSocket = new ServerSocket(port);
@@ -43,7 +53,26 @@ public class Server {
                 try {
                     final Socket requestSocket = serverSocket.accept();
                     System.out.println("Got new request from " + requestSocket.getInetAddress() + ":" + requestSocket.getPort());
-                    showContent(getRequestContent(requestSocket));
+
+                    String requestStr = getContent(requestSocket);
+
+                    showContent("origin content", requestStr);
+
+                    HTTPRequest request = new HTTPRequest(requestStr);
+                    System.out.println("Request Type: " + request.getRequestType());
+                    System.out.println("Request URI: " + request.getRequestUri());
+                    System.out.println("Request host's HTTP Ver: " + request.getHttpVersion());
+                    System.out.println("Request host: " + request.getHost());
+                    System.out.println("Request header: ");
+                    for (String line : request.getHeader()) {
+                        System.out.println(line);
+                    }
+
+                    HTTPResponse response = new HTTPResponse(request.getHttpVersion(), 200, "OK", "text/txt", System.in);
+                    showContent("response", response.toString());
+                    sendContent(requestSocket, response.toString());
+
+                    requestSocket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -59,11 +88,12 @@ public class Server {
         }
     }
 
-    public String getRequestContent(Socket requestSocket) {
+    public String getContent(Socket socket) {
         String content = new String("");
 
         // read request content
-        try (InputStream inputStream = requestSocket.getInputStream()) {
+        try {
+            InputStream inputStream = socket.getInputStream();
             while (inputStream.available() == 0)
                 Thread.sleep(100);  // wait until got content
 
@@ -80,10 +110,14 @@ public class Server {
         return content;
     }
 
-    public static void showContent(String content){
-        System.out.println("----------content----------");
-        System.out.println(content);
-        System.out.println("----------------------------");
+    public void sendContent(Socket socket, String content) {
+        try {
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(content.getBytes());
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public int getPort() {
